6bda45ac4e9fc6c1db4cdc84ed81a27c2239cc3a
Günseli Ünsal <gunseliunsal@hotmail.com>
2024-01-14 11:53:16 +0300

    add example education page

---
+++ lib/pages/education_page.dart
@@ -0,0 +1,51 @@
+import 'package:fish_radar/demos/home_page_card.dart';
+import 'package:flutter/material.dart';
+
+class CameraPage extends StatefulWidget {
+  const CameraPage({super.key});
+
+  @override
+  State<CameraPage> createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Center(
+            child: Text(
+          "Example Education Page",
+          style: TextStyle(color: Colors.white),
+        )),
+        Expanded(
+          child: ListView(
+            children: [
+              HomePageCard(
+                titleText: 'EXAMPLE EDUCATION PAGE',
+                subtitleText: 'Use our module',
+                leadingIcon: const Icon(Icons.school_outlined),
+                onTap: () {},
+              ),
+              HomePageCard(
+                titleText: 'EXAMPLE EDUCATION PAGE',
+                subtitleText: 'Open Google maps',
+                leadingIcon: const Icon(Icons.map_sharp),
+                onTap: () {},
+              ),
+              HomePageCard(
+                titleText: 'EXAMPLE EDUCATION PAGE',
+                subtitleText: 'See your favorite fish',
+                leadingIcon: const Icon(Icons.favorite_border_outlined),
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}

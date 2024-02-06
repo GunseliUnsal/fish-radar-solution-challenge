8ba06667e181c3237aafacacdd777c423a61740f
azranurvrdr <149239437+azranurvrdr@users.noreply.github.com>
2024-02-06 15:38:19 +0300

    Add files via upload

--- lib/demos/module_page_card.dart
+++ lib/demos/module_page_card.dart
@@ -12,64 +12,63 @@ class ModulePageCard extends StatelessWidget {
 
   final String titleText;
   final String subtitleText;
-  final Icon rowIcon;
+  final ImageIcon rowIcon;
   final Function() onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
+    return Center(
         child: Padding(
-          padding: const EdgeInsets.all(6.0),
-          child: SizedBox(
-            width: 180,
-            height: 180,
-            child: Card(
-              color: bottomNavbarColor,
-              child: InkWell(
-                splashColor: backgroundColor.withAlpha(50),
-                onTap: onTap,
-                child: Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      ListTile(
-                        iconColor: whiteColor,
-                        title: Text(
-                            titleText,
-                            style: TextStyle(
-                                color: whiteColor,
-                                fontSize: 11,
-                                fontWeight: FontWeight.bold,
-                            ),
-                        ),
-                        subtitle: Text(
-                          subtitleText,
-                          style: TextStyle(color: whiteColor, fontSize: 10),
-                        ),
-                        trailing: const Icon(Icons.arrow_forward_ios_outlined),
+      padding: const EdgeInsets.all(6.0),
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.46, // Adjust as needed
+        height: MediaQuery.of(context).size.width * 0.46, // Adjust as needed
+        child: Card(
+          color: bottomNavbarColor,
+          child: InkWell(
+            splashColor: backgroundColor.withAlpha(50),
+            onTap: onTap,
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    iconColor: whiteColor,
+                    title: Text(
+                      titleText,
+                      style: TextStyle(
+                        color: whiteColor,
+                        fontSize: 11,
+                        fontWeight: FontWeight.bold,
                       ),
-                      Flexible(
-                          child: Row(
-                            children: [
-                              Expanded(
-                                  child: Align(
-                                    alignment: Alignment.centerRight,
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(20.0),  //Remove const
-                                      child: rowIcon,
-                                    ),
-                                  ),
-                              ),
-                            ],
-                          ),
-                      )
-                    ],
+                    ),
+                    subtitle: Text(
+                      subtitleText,
+                      style: TextStyle(color: whiteColor, fontSize: 10),
+                    ),
+                    trailing: const Icon(Icons.arrow_forward_ios_outlined),
                   ),
-                ),
+                  Flexible(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: Padding(
+                              padding: const EdgeInsets.all(20.0),  //Remove const
+                              child: rowIcon,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
             ),
           ),
         ),
-    );
+      ),
+    ));
   }
 }
